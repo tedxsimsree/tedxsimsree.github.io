b661bf495654c044ea48685b06029b167bcf95d8
Shubham Gawande <ishubhamgawande@gmail.com>
2020-02-14 20:31:48 +0530

    About Section added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
+    
    
+    //Countdown Timer
+
+    var countDownDate = new Date("Feb 26, 2020 15:37:25").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("countDays").innerHTML = days;
+    document.getElementById("countHours").innerHTML = hours;
+    document.getElementById("countMins").innerHTML = minutes;
+    document.getElementById("countSecs").innerHTML = seconds;
+
+    
+    }, 1000);
+
     //Wow JS integration
     new WOW().init();
 
@@ -15,5 +44,8 @@ $(document).ready(function(){
         $('.navWrapper ul li').addClass('fadeInUp');
         $('.navWrapper ul li').addClass('delay-1s');
     })
+
+    
+    
     
     });
